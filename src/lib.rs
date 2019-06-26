dbe92b6133ce4953d3d0ab8c30c764f90e6d9a40
Piet Geursen <pietgeursen@gmail.com>
2019-06-26 13:04:24 +1200

    Test large n

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn lipmaa(n: u64) -> u64 {
+pub fn lipmaa(n: u32) -> u32 {
     if n < 1 {
         panic!("n must be larger than 0")
     }
@@ -9,30 +9,30 @@ pub fn lipmaa(n: u64) -> u64 {
 
     let k = find_k(n);
 
-    if n == ((3u64.pow(k as u32)) - 1) / 2 {
-        n - (3u64.pow(k as u32 - 1))
+    if n == (((3u64.pow(k)) - 1) / 2) as u32 {
+        n - (3u32.pow(k - 1))
     } else {
-        n - (((3u64.pow(g(n) as u32)) - 1) / 2)
+        n - (((3u64.pow(g(n))) - 1) / 2) as u32
     }
 }
 
-fn g(n: u64) -> u64 {
+fn g(n: u32) -> u32 {
     let k = find_k(n);
-    if n == ((3u64.pow(k as u32)) - 1) / 2 {
+    if n == (((3u64.pow(k)) - 1) / 2) as u32 {
         k
     } else {
         let k = find_new_k(n);
-        g(n - (((3u64.pow(k as u32 - 1)) - 1) / 2))
+        g(n - (((3u64.pow(k - 1)) - 1) / 2) as u32)
     }
 }
 
-fn find_k(n: u64) -> u64 {
-    f64::log((n as f64) * 2.0 + 1.0, 3.0) as u64
+fn find_k(n: u32) -> u32 {
+    f64::log((n as f64) * 2.0 + 1.0, 3.0) as u32
 }
 
-fn find_new_k(n: u64) -> u64 {
+fn find_new_k(n: u32) -> u32 {
     let k = find_k(n) + 1;
-    if n < (3u64.pow(k as u32) - 1) / 2 {
+    if n < ((3u64.pow(k) - 1) / 2) as u32 {
         k
     } else {
         k + 1
@@ -43,26 +43,6 @@ fn find_new_k(n: u64) -> u64 {
 mod tests {
 
     use crate::{find_k, lipmaa};
-
-    #[test]
-    fn find_k_is_ok() {
-        assert_eq!(find_k(1), 1);
-        assert_eq!(find_k(3), 1);
-        assert_eq!(find_k(4), 2);
-        assert_eq!(find_k(5), 2);
-        assert_eq!(find_k(12), 2);
-        assert_eq!(find_k(13), 3);
-        assert_eq!(find_k(40), 4);
-    }
-
-    #[test]
-    fn lipmaa_spines() {
-        let actual_expecteds = [(4, 1), (13, 4), (40, 13)];
-
-        actual_expecteds
-            .iter()
-            .for_each(|(n, expected)| assert_eq!(lipmaa(*n), *expected))
-    }
 
     #[test]
     fn lipmaa_is_ok() {
@@ -108,6 +88,32 @@ mod tests {
             (39, 26),
             (40, 13),
         ];
+
+        actual_expecteds
+            .iter()
+            .for_each(|(n, expected)| assert_eq!(lipmaa(*n), *expected))
+    }
+
+    #[test]
+    fn largest_n() {
+        assert_eq!(lipmaa(core::u32::MAX), 4294967294);
+    }
+
+    #[test]
+    fn find_k_is_ok() {
+        assert_eq!(find_k(1), 1);
+        assert_eq!(find_k(3), 1);
+        assert_eq!(find_k(4), 2);
+        assert_eq!(find_k(5), 2);
+        assert_eq!(find_k(12), 2);
+        assert_eq!(find_k(13), 3);
+        assert_eq!(find_k(40), 4);
+        assert_eq!(find_k(core::u32::MAX), 20);
+    }
+
+    #[test]
+    fn lipmaa_spines() {
+        let actual_expecteds = [(4, 1), (13, 4), (40, 13)];
 
         actual_expecteds
             .iter()
